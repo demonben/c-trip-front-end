740b0b6a865c749865dddd338cb41808ebc18469
Elisha Avior <avior@Elishas-MacBook-Air.local>
2021-05-11 13:35:04 +0300

    google verif

--- src/utils/clientId.js
+++
@@ -1,5 +0,0 @@
-const clientId =
-  '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com'; // this ID should be changed; it's not ours. Working on it (Elisha).
-// '1034320855669-61p07u3hk9q1d25tn18ab1pb3i3fhjs4.apps.googleusercontent.com'; // Elisha's ID, not working yet
-
-export default clientId;
