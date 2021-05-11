ec42a518437f4e412d449674662471f1b05b8888
Elisha Avior <avior@Elishas-MacBook-Air.local>
2021-05-11 18:36:17 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import LoginHooks from './components/LoginHooks';
 import LogoutHooks from './components/LogoutHooks';
+import { apiTest } from './lib/api';
 
 function App() {
+  useEffect(() => {
+    getApiTest();
+  }, []);
+
+  const getApiTest = async () => {
+    try {
+      const result = await apiTest();
+      console.log(result);
+    } catch (error) {}
+  };
+
   return (
     <div className="App">
       <h2>The Components way</h2>
