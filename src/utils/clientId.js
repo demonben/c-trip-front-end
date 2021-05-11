48d0e7e14ece40f4733542d477e803c2b8e92ace
Elisha Avior <avior@Elishas-MacBook-Air.local>
2021-05-11 15:46:09 +0300

    Start

--- src/utils/clientId.js
+++ src/utils/clientId.js
@@ -1,5 +1,7 @@
 const clientId =
-  // '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com'; // this ID should be changed; it's not ours. Working on it (Elisha).
-  '1034320855669-61p07u3hk9q1d25tn18ab1pb3i3fhjs4.apps.googleusercontent.com'; // Elisha's ID, not working yet
+  '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com'; // this ID should be changed; it's not ours. Working on it (Elisha). This ID works with localhost but not on the deployed version (mismatch between domain and client account)
+// '1034320855669-61p07u3hk9q1d25tn18ab1pb3i3fhjs4.apps.googleusercontent.com'; // Elisha's ID, not working yet. Our deployed domain has been verified, but updating it can take up to 72 hours:
+
+// When you have successfully verified your domain, you're ready to create new users for your Identity account. Note: The process of adding a DNS record only takes 10 minutes, but verification can take up to 72 hours and is controlled by the domain host.
 
 export default clientId;
