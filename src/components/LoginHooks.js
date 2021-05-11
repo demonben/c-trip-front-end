db3b36d79108b613d8b89e5c581e391bf31a91e4
Elisha Avior <avior@Elishas-MacBook-Air.local>
2021-05-11 13:50:59 +0300

    Google login

---
+++ src/components/LoginHooks.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useGoogleLogin } from 'react-google-login';
+import clientId from '../utils/clientId';
+
+// refresh token
+import { refreshTokenSetup } from '../utils/refreshToken';
+
+function LoginHooks() {
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+    );
+    refreshTokenSetup(res);
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(`Failed to login.`);
+  };
+
+  const { signIn } = useGoogleLogin({
+    onSuccess,
+    onFailure,
+    clientId,
+    isSignedIn: true,
+    accessType: 'offline',
+    // responseType: 'code',
+    // prompt: 'consent',
+  });
+
+  return (
+    <button onClick={signIn} className="button">
+      <img src="icons/google.svg" alt="google login" className="icon"></img>
+
+      <span className="buttonText">Sign in with Google</span>
+    </button>
+  );
+}
+
+export default LoginHooks;
---
+++ src/components/Logout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+import clientId from '../utils/clientId';
+
+function Logout() {
+  const onSuccess = () => {
+    console.log('Logout made successfully');
+    alert('Logout made successfully ✌');
+  };
+
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}></GoogleLogout>
+    </div>
+  );
+}
+
+export default Logout;
