ba209749d533e53bdad7ebd9336411e30680891f
Alina Novofastovskaia <alinanovof@gmail.com>
2021-05-13 21:06:22 +0300

    Navbar, Home Page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,63 @@
-import React from 'react'
-import Login from './Login'
-import LoginHooks from './LoginHooks'
-import Logout from './Logout'
-import LogoutHooks from './LogoutHooks'
-import Search from './Search'
-import Home from './pages/Home'
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
-import {
-    NavLink, Link
-} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { AppBar, Toolbar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
+const useStyles = makeStyles(theme => ({
+    navLink: {
+      marginRight: theme.spacing(5),
+      textDecoration: 0,
+      fontWeight: "bold",
+      color: "black",
+    },
+    title: {
+      flexGrow: 1,
+      marginRight: theme.spacing(40)
+    },
+    btn:{
+        marginRight: theme.spacing(10),
+        marginLeft: theme.spacing(3),
+        border: "2px solid black",
+        padding: "0.5rem",
+        paddingLeft: "1.5rem",
+        paddingRight: "1.5rem",
+        borderRadius: "2px",
+        textDecoration: 0,
+        fontWeight: "bold",
+        color: "black",
+    },
+    bar:{
+        boxShadow: "none"
+    }
+  }));
 
 const NavBar = () => {
-    return (
-        <div>
-           
-                
-                <Nav className="mr-auto">
-                    <Link className="nav-item" exact to="/">
-                        Home
-                    </Link>
-                </Nav>
-                <Nav className="mr-auto">
-                    <Link className="nav-item" to="/search">
-                        Search
-                    </Link>
-                </Nav>
-                <Nav className="mr-auto">
-                    <Link className="nav-item" to="/login">
-                    <LoginHooks />
-                    <LogoutHooks />
-                    </Link>
-                </Nav>
-                <Form inline></Form>
-
-            
-            <br />
-            <Search />
-            <Home />
-
-            {/* <p>The Components way</p>
+    const classes = useStyles();
+  return (
+    <AppBar position="static" color="transparent" className={classes.bar}>
+      <Toolbar>
+        <h3 className={classes.title}>cTrip</h3>
+        <NavLink className={classes.navLink} exact to="/">
+          Home
+        </NavLink>
+        <NavLink className={classes.navLink} to="/">
+          My Trips
+        </NavLink>
+        <NavLink className={classes.navLink} to="/search">
+          Search
+        </NavLink>
+        <NavLink className={classes.btn} to="/login">
+          Login
+        </NavLink>
+        {/* <p>The Components way</p>
             <Login />
             <br />
             <Logout /> */}
-        </div>
-    )
-}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,64 +1,25 @@
-// import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-import { TextField } from "@material-ui/core";
+const useStyles = makeStyles(theme => ({
+    headText: {
+     fontSize: "2rem",
+	 wordWrap: "break-word"
+    },
+	subHead: {
+		
+	}
+  }));
 
 export default function Home() {
-	// const { user } = useContext(setUserTokenContext);
-	// if (!user) {
-	return (
-		<>
-			{/* <form  noValidate>
-  <TextField
-    id="time"
-    label="Alarm clock"
-    type="time"
-    defaultValue="07:30"
-    // className={classes.textField}
-    InputLabelProps={{
-      shrink: true,
-    }}
-    inputProps={{
-      step: 300, // 5 min
-    }}
-  />
-</form> */}
-			{/* <Segment style={{ padding: "2em 0em" }} vertical>
-					<Grid centered stackable>
-						<Grid.Column width={14}> */}
-			<p>"Welcome, Friend on cTrip!"</p>
-
-
-			{/* <Image
-								centered
-								src={
-									"https://image.freepik.com/free-photo/group-pet_87557-4334.jpg"
-								}
-								size="large"
-							/>
-
-							<Header as="h3" style={{ fontSize: "2em" }}>
-								Why it's a great idea to adopt a pet?
-							</Header> */}
-			{/* 
-							// <p style={{ fontSize: "1.43em" }}>
-							// 	Pet adoption is quickly becoming the preferred way to find a new
-							// 	dog, puppy, cat or kitten. Best of all, there are so many
-							// 	benefits when you adopt a dog or adopt a cat over buying. There
-							// 	are many health benefits of owning a pet. They can increase
-							// 	opportunities to exercise, get outside, and socialize. Regular
-							// 	walking or playing with pets can decrease blood pressure,
-							// 	cholesterol levels, and triglyceride levels. Pets can help
-							// 	manage loneliness and depression by giving us companionship.
-							// </p> */}
-
-			{/* {/* <Link to="/SearchAFriend">
-								<Button color="orange" icon="plus" widths="7">
-									<p>Search pets</p>
-								</Button>
-							</Link>
-						</Grid.Column>
-					</Grid>
-				</Segment> */}
-		</>
-	)
+	const classes = useStyles();
+  return (
+    <>
+      <h1 className={classes.headText}>All your adventures in one place</h1>
+	  <h5 className={classes.subHead}>Find hotels. Write some notes. Have a safe trip.</h5>
+	  <Button variant="contained" color="primary" href="/search">Explore</Button>
+	  <img src="https://res.cloudinary.com/alinanovof/image/upload/c_lfill,h_883,w_1589,x_0/v1620928598/pexels-tatiana-614484_wcyyy3.jpg"/>
+	</>
+  );
 }
