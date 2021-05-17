f15d73ba65c40873dbe4cff97ccad090da6e87bb
Dmitrii Benenson <71938599+demonben@users.noreply.github.com>
2021-05-17 11:02:35 +0300

    develope consuming data from back

--- src/components/pages/search/HotelItem.jsx
+++ src/components/pages/search/HotelItem.jsx
@@ -9,24 +9,27 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 export const HotelItem = ({ hotel, hotelMockInfo }) => {
-
   return (
     <div>
       <div className="hotelItem">{hotel}</div>
       <Card>
         <CardActionArea>
-          <img src="https://images.unsplash.com/photo-1581974206967-93856b25aa13?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzB8fGhvdGVsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+          <img
+            src={hotelMockInfo.images[0]}
+            alt="hotel photo"
+            width="300"
+            height="200"
+          />
           <CardMedia
             src="https://exp.cdn-hotels.com/hotels/1000000/20000/16400/16366/2dac0c53_z.jpg"
             title="Contemplative Reptile"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
-              Lizard
+              {hotelMockInfo.name}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              {hotelMockInfo.tagLine}
             </Typography>
           </CardContent>
         </CardActionArea>
--- src/components/pages/search/ListOfResults.jsx
+++ src/components/pages/search/ListOfResults.jsx
@@ -3,6 +3,7 @@ import {HotelItem} from './HotelItem'
 
 export const ListOfResults = ({ hotelsList, hotelMockInfo }) => {
   console.log(hotelMockInfo);
+  console.log(hotelMockInfo.images[0]);
   return (
     <div className="hotelsList">
       {hotelsList.map((hotel) => (
