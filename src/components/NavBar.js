61c137b7558d8d002f13f9e3910821b0c3fd980e
Yulia Kitan <yuliakitan@gmail.com>
2021-05-18 20:57:17 +0300

    try

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -85,7 +85,7 @@ const NavBar = () => {
         <NavLink className={classes.navLink} exact to="/">
           Home
         </NavLink>
-        {localStorage.authToken && <NavLink className={classes.navLink} to="/">
+        {!localStorage.authToken && <NavLink className={classes.navLink} to="/myTrips">
           My Trips
         </NavLink>}
         <NavLink className={classes.navLink} to="/search">
